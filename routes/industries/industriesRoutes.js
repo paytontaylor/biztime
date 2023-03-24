f4ad03f090c4f800e2d79c1273d795ab12be3709
paytontaylor <payton@paytontaylor.net>
2023-03-24 01:51:49 -0500

    industries routes uploaded and invoices routes updated for payments taken

--- routes/industries/industriesRoutes.js
+++ routes/industries/industriesRoutes.js
@@ -1,16 +1,56 @@
 const express = require("express");
-const ExpressError = require("../../ExpressError");
+const errorCheck = require("../../helpers/errorCheck");
 const db = require("../../db");
 const router = new express.Router();
 
 router.get("/", async (req, res, next) => {
-  const result = await db.query(`
-  SELECT i.industry, ci.company_code 
-  FROM industries AS i
-  JOIN companies_industries AS ci
-  ON ci.industry_code = i.code 
-  `);
-  return res.send(result.rows)
+  try {
+    const result = await db.query(`
+    SELECT i.code, i.industry, ci.company_code
+    FROM industries AS i
+    JOIN companies_industries AS ci
+    ON ci.industry_code = i.code
+    `);
+    errorCheck(result)
+    const industryMap = {};
+
+    for (const row of result.rows) {
+      if (!industryMap[row.code]) {
+        industryMap[row.code] = {
+          code: row.code,
+          industry: row.industry,
+          companies: [],
+        };
+      }
+      industryMap[row.code].companies.push(row.company_code);
+    }
+
+    return res.send(Object.values(industryMap));
+  } catch (err){
+    return next(err);
+  }
 });
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { code, industry } = req.body;
+    const result = await db.query(`INSERT INTO industries (code, industry) VALUES ($1, $2) RETURNING code, industry`, [code, industry]);
+    errorCheck(result);
+    return res.status(201).json({ industry: result.rows[0] });
+  } catch (err) {
+    return next(err)
+  }
+});
+
+router.post("/:industry_code", async (req, res, next) => {
+  try {
+    const { company_code } = req.body;
+    const result = await db.query(`INSERT INTO companies_industries (company_code, industry_code) VALUES ($1,$2) RETURNING company_code, industry_code`, [company_code, req.params.industry_code]);
+    errorCheck(result);
+    return res.status(201).json({ industry: result.rows[0] });
+  } catch (err) {
+    return next(err)
+  }
+})
 
 module.exports = router;
--- routes/invoices/invoicesRoutes.js
+++ routes/invoices/invoicesRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
+const errorCheck = require("../../helpers/errorCheck");
 const router = express.Router();
-const ExpressError = require("../../ExpressError");
 const db = require("../../db");
 
 router.get("/", async (req, res, next) => {
@@ -13,8 +13,8 @@ router.get("/:id", async (req, res, next) => {
     const result = await db.query(`SELECT * FROM invoices WHERE id=$1`,[req.params.id]);
     const { id, amt, paid, add_date, paid_date, comp_code } = result.rows[0];
     const company = await db.query(`SELECT * FROM companies WHERE code=$1`, [comp_code])
-    
-    if (result.rows.length === 0) throw new ExpressError("Invoice Not Found", 404);
+    errorCheck(result);
+    errorCheck(company);
     return res.json({invoice: {id, amt, paid, add_date, paid_date, company: company.rows[0]}});
   } catch (err){
     return next(err)
@@ -30,9 +30,27 @@ router.post("/", async (req, res, next) => {
 router.put("/:id", async(req, res, next) => {
   try {
     let { amt, paid } = req.body;
-    const result = await db.query(`UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING id, comp_code, amt, paid, add_date, paid_date`, [amt, req.params.id]);
-    if (result.rows.length === 0) throw new ExpressError("Invoice Not Found", 404);
-    return res.json({invoice: result.rows[0]})
+    let id = req.params.id;
+    let paidDate = null;
+
+    const currResult = await db.query(
+      `SELECT paid
+       FROM invoices
+       WHERE id = $1`, [id]);
+    errorCheck(currResult);
+    
+    const currPaidDate = currResult.rows[0].paid_date;
+
+    if (!currPaidDate && paid){
+      paidDate = new Date();
+    } else if (!paid) {
+      paidDate = null;
+    } else {
+      paidDate = currPaidDate;
+    }
+    const result = await db.query(`UPDATE invoices SET amt=$1, paid=$2, paid_date=$3 WHERE id=$4 RETURNING id, comp_code, amt, paid, add_date, paid_date`, [amt, paid, paidDate, id]);
+    
+    return res.json({"invoice": result.rows[0]});
   } catch (err) {
     return next(err)
   }
